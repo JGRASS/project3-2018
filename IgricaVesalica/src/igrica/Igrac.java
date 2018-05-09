093ecb312c1443333787ee2fbc425dba52d55c5f
Rasic10 <nemanja.rasic10@gmail.com>
2018-05-09 03:11:30 +0200

    Uradjen JavaDoc za klase Igrac i Igrica

--- IgricaVesalica/src/igrica/Igrac.java
+++ IgricaVesalica/src/igrica/Igrac.java
@@ -5,22 +5,55 @@ import java.util.Random;
 import bazaPodataka.BazaReci;
 import interfejs.IgracInterfejs;
 
+/**
+ * Klasa o igracu.
+ * 
+ */
+
 public class Igrac implements IgracInterfejs {
-	
+
+	/**
+	 * Privatni atribut predstavlja igricu koja je inicijalizovana.
+	 */
 	private Igrica igrica;
-	
+
+	/**
+	 * Privatni atribut predstavlja indeks iz niza stringa odredjene kategorije koju
+	 * je igrac odabrao.
+	 */
 	private int indeksReci;
-	
-	public int getIndexReci() {
+
+	/**
+	 * Metoda vraca indeks reci koju igrac pogadja iz odredjene kategorije.
+	 * 
+	 * @return indeksReci
+	 */
+	public int getIndeksReci() {
 		return indeksReci;
 	}
 
+	/**
+	 * @param kategorija
+	 *            Kategorija reci koju je igrac odabrao.
+	 * 
+	 * @return igrica.vratiRecZaPrikaz().
+	 * 
+	 *         Metoda vraca niz karaktera koji se sastoji od X na mestima na koja se
+	 *         nalaze slova i praznim mestima ako ima vise od jedne reci.
+	 */
 	@Override
 	public char[] pokreniIgricu(String kategorija) {
 		igrica = new Igrica(zadajRecIgracu(kategorija));
 		return igrica.sakrivenaRecNaPocetkuIgre();
 	}
-	
+
+	/**
+	 * @param kategorija
+	 *            Kategorija reci koju je igrac odabrao.
+	 * @return reciIzKategorije[indeksReci] U zavisnosti od izabrane kategorije bira
+	 *         se jedan random rec na indeksu indeksReci iz te kategorije i data rec
+	 *         se vraca kao String.
+	 */
 	private String zadajRecIgracu(String kategorija) {
 		String[] reciIzKategorije = null;
 		switch (kategorija.toLowerCase()) {
@@ -53,10 +86,14 @@ public class Igrac implements IgracInterfejs {
 		return reciIzKategorije[indeksReci];
 	}
 
+	/**
+	 * @return randomBroj Metoda generise random broj koji oznacava indeks reci koju
+	 *         ce igrac pokagadjati.
+	 */
 	private int odaberiRandomBroj() {
 		Random rand = new Random();
-		int  n = rand.nextInt(15); // mozda treba + 1 da bi se koristio i 15. zanimanje
-		return n;
+		int randomBroj = rand.nextInt(15); // mozda treba + 1 da bi se koristio i 15. zanimanje
+		return randomBroj;
 	}
 
 }
--- IgricaVesalica/src/igrica/Igrica.java
+++ IgricaVesalica/src/igrica/Igrica.java
@@ -2,28 +2,63 @@ package igrica;
 
 import interfejs.IgricaInterfejs;
 
+/**
+ * Klasa o igrici.
+ * 
+ */
 public class Igrica implements IgricaInterfejs {
-	
+
+	/**
+	 * Atribut predstavlja rec koju igrac treba da pogodi.
+	 */
 	private String recKojaSeTrazi;
-	
+
+	/**
+	 * Atribut pokazuje koliko puta je igrac promasio u odabiru slova.
+	 */
 	private int brojPromasaja;
-	
+
+	/**
+	 * Atribut predstavlja rec koja je delimicno pogodjena.
+	 */
 	private char[] recSaOtkrivenimSlovima;
-	
+
+	/**
+	 * @param zadataRec
+	 *            Kontstruktor inicijalizuje klasu i uvodi novu zadatu rec, pocinje
+	 *            da broji broj promasaja i inicijalizuje rec koja ce biti prikazana
+	 *            nakon pokusaja pogadjanja.
+	 */
 	public Igrica(String zadataRec) {
 		this.recKojaSeTrazi = zadataRec;
 		brojPromasaja = 0;
 		recSaOtkrivenimSlovima = sakrivenaRecNaPocetkuIgre();
 	}
-	
+
+	/**
+	 * @return recKojaSeTrazi Metoda vraca rec za kojom igrac traga.
+	 */
 	public String getRecKojaSeTrazi() {
 		return recKojaSeTrazi;
 	}
 
+	/**
+	 * @return brojPromasaja Metoda vraca broj promasaja pri pogadjanju igraca.
+	 */
 	public int getBrojPromasaja() {
 		return brojPromasaja;
 	}
 
+	/**
+	 * @param slovo
+	 * @return recSaOtkrivenimSlovima.
+	 * 
+	 *         Metoda proverava da li se na nekom mestu u nizu karaktera nalazi
+	 *         slovo koje je prosledjeno kao parametar. Ukoliko jeste, onda se
+	 *         vrednost elemenata na tim mestima u nizu menja na to slovo i na kraju
+	 *         metoda vraca izmenjenu rec za prikaz u obliku niza u protivnom se
+	 *         povecava broj promasaja.
+	 */
 	@Override
 	public char[] dodajSlovo(char slovo) {
 		if (recKojaSeTrazi.toUpperCase().indexOf(slovo) != -1) {
@@ -39,6 +74,12 @@ public class Igrica implements IgricaInterfejs {
 		}
 	}
 
+	/**
+	 * @return recZaPrikaz.toCharArray() Metoda vraca rec za prikaz nakon
+	 *         inicijalizacije igrice. Napravi se niz koji se sastoji od 'X' na
+	 *         mestima gde su slova i ' ' u slucaju razmaka izmedju dve reci i sam
+	 *         niz ce biti prikazan igracu da bi se pokazalo koliko rec ima slova.
+	 */
 	@Override
 	public char[] sakrivenaRecNaPocetkuIgre() {
 		String recZaPrikaz = "";
@@ -51,6 +92,12 @@ public class Igrica implements IgricaInterfejs {
 		return recZaPrikaz.toCharArray();
 	}
 
+	/**
+	 * Ukoliko je rec koju je korisnik uneo jednaka trazenoj reci onda metoda vraca
+	 * true, a u suprotnom vraca false.
+	 * 
+	 * @return boolean
+	 */
 	@Override
 	public boolean daLiJePogodjenaRec() {
 		for (int i = 0; i < recSaOtkrivenimSlovima.length; i++) {
@@ -60,6 +107,12 @@ public class Igrica implements IgricaInterfejs {
 		return true;
 	}
 
+	/**
+	 * U slucaju da je broj promasaja jednak 6 onda metoda vraca true i igrac je
+	 * izgubio, u protivnom vraca false jer igrac ima pravo da jos traga za pojmom.
+	 * 
+	 * @return boolean
+	 */
 	@Override
 	public boolean daLiSuPotroseniSviPokusaji() {
 		if (getBrojPromasaja() >= 6) {
