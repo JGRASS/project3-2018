8881cbf832404fd68d37b2bfcbbac89d21ca41b6
DusanBMitrovic <dule.mitrovic000@gmail.com>
2018-05-10 23:43:36 +0200

    Napisana dokumentacija za test klase

--- IgricaVesalica/test/test/TestBazaReci.java
+++ IgricaVesalica/test/test/TestBazaReci.java
@@ -6,87 +6,156 @@ import org.junit.Test;
 
 import bazaPodataka.BazaReci;
 
+/**
+ * Klasa koja testira klasu BazaReci
+ * 
+ * @author Dusan Mitrovic
+ *
+ */
 public class TestBazaReci {
 
+	/**
+	 * Metoda testira get metodu za kategoriju zivotinje kada je u pitanju duzina
+	 * niza reci koje spadaju u tu kategoriju
+	 */
 	@Test
-	public void testGetZivotinjeVelicina(){
-		assertEquals(15,BazaReci.getZivotinje().length);		
-	}
-	
-	@Test
-	public void testGetDrzaveVelicina() {
-		assertEquals(15,BazaReci.getDrzave().length);
-	}
-	
-	@Test
-	public void testGetEvropskiGlavniGradoviVelicina() {
-		assertEquals(15,BazaReci.getEvropskiGlavniGradovi().length);
-	}
-	
-	@Test
-	public void testGetMuzickiInstrumentiVelicina() {
-		assertEquals(15,BazaReci.getMuzickiInstrumenti().length);
-	}
-	
-	@Test
-	public void testGetZanimanjaVelicina() {
-		assertEquals(15,BazaReci.getZanimanja().length);
-	}
-	
-	@Test
-	public void testGetPovrceVelicina() {
-		assertEquals(15,BazaReci.getPovrce().length);
-	}
-	
-	@Test
-	public void testGetVoceVelicina() {
-		assertEquals(15,BazaReci.getVoce().length);
-	}
-	
-	@Test
-	public void testGetSportoviVelicina() {
-		assertEquals(15,BazaReci.getSportovi().length);
+	public void testGetZivotinjeVelicina() {
+		assertEquals(15, BazaReci.getZivotinje().length);
 	}
 
-	
+	/**
+	 * Metoda testira get metodu za kategoriju drzave kada je u pitanju duzina niza
+	 * reci koje spadaju u tu kategoriju
+	 */
+	@Test
+	public void testGetDrzaveVelicina() {
+		assertEquals(15, BazaReci.getDrzave().length);
+	}
+
+	/**
+	 * Metoda testira get metodu za kategoriju evropski glavni gradovi kada je u
+	 * pitanju duzina niza reci koje spadaju u tu kategoriju
+	 */
+	@Test
+	public void testGetEvropskiGlavniGradoviVelicina() {
+		assertEquals(15, BazaReci.getEvropskiGlavniGradovi().length);
+	}
+
+	/**
+	 * Metoda testira get metodu za kategoriju muzicki instrumenti kada je u pitanju
+	 * duzina niza reci koje spadaju u tu kategoriju
+	 */
+	@Test
+	public void testGetMuzickiInstrumentiVelicina() {
+		assertEquals(15, BazaReci.getMuzickiInstrumenti().length);
+	}
+
+	/**
+	 * Metoda testira get metodu za kategoriju zanimanja kada je u pitanju duzina
+	 * niza reci koje spadaju u tu kategoriju
+	 */
+	@Test
+	public void testGetZanimanjaVelicina() {
+		assertEquals(15, BazaReci.getZanimanja().length);
+	}
+
+	/**
+	 * Metoda testira get metodu za kategoriju povrce kada je u pitanju duzina niza
+	 * reci koje spadaju u tu kategoriju
+	 */
+	@Test
+	public void testGetPovrceVelicina() {
+		assertEquals(15, BazaReci.getPovrce().length);
+	}
+
+	/**
+	 * Metoda testira get metodu za kategoriju voce kada je u pitanju duzina niza
+	 * reci koje spadaju u tu kategoriju
+	 */
+	@Test
+	public void testGetVoceVelicina() {
+		assertEquals(15, BazaReci.getVoce().length);
+	}
+
+	/**
+	 * Metoda testira get metodu za kategoriju sportovi kada je u pitanju duzina
+	 * niza reci koje spadaju u tu kategoriju
+	 */
+	@Test
+	public void testGetSportoviVelicina() {
+		assertEquals(15, BazaReci.getSportovi().length);
+	}
+
+	/**
+	 * Metoda testira get metodu za kategoriju zivotinje kada je u pitanju peta rec
+	 * u nizu reci koje pripadaju toj kategoriji
+	 */
 	@Test
 	public void testGetZivotinjeVratiPetu() {
-		assertEquals("Zlatna ribica" , BazaReci.getZivotinje()[4]);
+		assertEquals("Zlatna ribica", BazaReci.getZivotinje()[4]);
 	}
-	
+
+	/**
+	 * Metoda testira get metodu za kategoriju drzave kada je u pitanju peta rec u
+	 * nizu reci koje pripadaju toj kategoriji
+	 */
 	@Test
 	public void testGetDrzaveVratiPetu() {
-		assertEquals("Italija" , BazaReci.getDrzave()[4]);
+		assertEquals("Italija", BazaReci.getDrzave()[4]);
 	}
-	
+
+	/**
+	 * Metoda testira get metodu za kategoriju evropski glavni gradovi kada je u
+	 * pitanju peta rec u nizu reci koje pripadaju toj kategoriji
+	 */
 	@Test
 	public void testGetEvropskiGlavniGradoviVratiPetu() {
-		assertEquals("Riga" , BazaReci.getEvropskiGlavniGradovi()[4]);
+		assertEquals("Riga", BazaReci.getEvropskiGlavniGradovi()[4]);
 	}
-	
+
+	/**
+	 * Metoda testira get metodu za kategoriju muzicki instrumenti kada je u pitanju
+	 * peta rec u nizu reci koje pripadaju toj kategoriji
+	 */
 	@Test
 	public void testGetMuzickiInstrumentiVratiPetu() {
-		assertEquals("Oboa" , BazaReci.getMuzickiInstrumenti()[4]);
+		assertEquals("Oboa", BazaReci.getMuzickiInstrumenti()[4]);
 	}
-	
+
+	/**
+	 * Metoda testira get metodu za kategoriju zanimanja kada je u pitanju peta rec
+	 * u nizu reci koje pripadaju toj kategoriji
+	 */
 	@Test
 	public void testGetZanimanjaVratiPetu() {
-		assertEquals("Pilot" , BazaReci.getZanimanja()[4]);
+		assertEquals("Pilot", BazaReci.getZanimanja()[4]);
 	}
-	
+
+	/**
+	 * Metoda testira get metodu za kategoriju povrce kada je u pitanju peta rec u
+	 * nizu reci koje pripadaju toj kategoriji
+	 */
 	@Test
 	public void testGetPovrceVratiPetu() {
-		assertEquals("Blitva" , BazaReci.getPovrce()[4]);
+		assertEquals("Blitva", BazaReci.getPovrce()[4]);
 	}
-	
+
+	/**
+	 * Metoda testira get metodu za kategoriju voce kada je u pitanju peta rec u
+	 * nizu reci koje pripadaju toj kategoriji
+	 */
 	@Test
 	public void testGetVoceVratiPetu() {
-		assertEquals("Kivi" , BazaReci.getVoce()[4]);
+		assertEquals("Kivi", BazaReci.getVoce()[4]);
 	}
-	
+
+	/**
+	 * Metoda testira get metodu za kategoriju sportovi kada je u pitanju peta rec u
+	 * nizu reci koje pripadaju toj kategoriji
+	 */
 	@Test
 	public void testGetSportoviVratiPetu() {
-		assertEquals("Stoni tenis" , BazaReci.getSportovi()[4]);
+		assertEquals("Stoni tenis", BazaReci.getSportovi()[4]);
 	}
 
 }
--- IgricaVesalica/test/test/TestIgrica.java
+++ IgricaVesalica/test/test/TestIgrica.java
@@ -6,49 +6,83 @@ import org.junit.Test;
 
 import igrica.Igrica;
 
+/**
+ * Klasa koja testira klasu Igrica
+ * 
+ * @author Dusan Mitrovic
+ *
+ */
 public class TestIgrica {
 
+	/**
+	 * Metoda testira get metodu od atributa koji predstavlja rec koju korisnik
+	 * treba da pogodi
+	 */
 	@Test
 	public void testGetRecKojaSeTraziInicijalizacija() {
 		Igrica ig = new Igrica("Zvezda");
 		assertEquals("Zvezda", ig.getRecKojaSeTrazi());
 	}
 
+	/**
+	 * Metoda testira get metodu atributa koji predstavlja broj promasaja koji
+	 * korisnik ima
+	 */
 	@Test
 	public void testGetBrojPromasajaInicijalizacija() {
 		Igrica ig = new Igrica("Zvezda");
 		assertEquals(0, ig.getBrojPromasaja());
 	}
-	
+
+	/**
+	 * Metoda testira set metodu atributa koji predstavlja broj promasaja koji
+	 * korisnik ima
+	 */
 	@Test
 	public void testSetBrojPromasaja() {
 		Igrica ig = new Igrica("Zvezda");
 		ig.setBrojPromasaja(10);
 		assertEquals(10, ig.getBrojPromasaja());
 	}
-	
+
+	/**
+	 * Metoda testira da li se prikazuje isti broj crtica koliko slova ima zadata
+	 * rec
+	 */
 	@Test
 	public void testSakrivenaRecNaPocetkuIgre() {
 		Igrica ig = new Igrica("Zvezda");
 		char[] rec = ig.sakrivenaRecNaPocetkuIgre();
 		String r = String.valueOf(rec);
-		
+
 		assertEquals("______", r);
 	}
-	
+
+	/**
+	 * Metoda testira metodu koja proverava da li je korisnik pogodio sva slova u
+	 * zadatoj reci
+	 */
 	@Test
 	public void testDaLiJePogodjenarecInicijalizacija() {
 		Igrica ig = new Igrica("Zvezda");
 		assertEquals(false, ig.daLiJePogodjenaRec());
 	}
-	
+
+	/**
+	 * Metoda testira metodu koja proverava da li je korisnik iskoristio sve
+	 * pokusaje kada je broj pokusaja veci od dozvoljenog
+	 */
 	@Test
 	public void testDaLiSuPotroseniSviPokusaji() {
 		Igrica ig = new Igrica("Zvezda");
 		ig.setBrojPromasaja(7);
 		assertEquals(true, ig.daLiSuPotroseniSviPokusaji());
 	}
-	
+
+	/**
+	 * Metoda testira metodu koja proverava da li je korisnik iskoristio sve
+	 * pokusaje kada unese slova koja se ne nalaze u zadatoj reci
+	 */
 	@Test
 	public void testDaLiSuPotroseniSviPokusajiPriDodavanjuSlova() {
 		Igrica ig = new Igrica("Zvezda");
@@ -60,9 +94,8 @@ public class TestIgrica {
 		ig.dodajSlovo('c');
 		ig.dodajSlovo('g');
 		ig.dodajSlovo('l');
-		assertEquals(true,ig.daLiSuPotroseniSviPokusaji());
-		
+		assertEquals(true, ig.daLiSuPotroseniSviPokusaji());
+
 	}
-	
-	
+
 }
